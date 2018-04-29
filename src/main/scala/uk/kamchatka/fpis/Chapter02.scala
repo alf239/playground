97cca3e6278b9f38b4f09519d42a985acbff4dca
Alexey Filippov <afilippov@acm.org>
2018-04-29 20:22:56 +0100

    FPiS ch.2 naming

--- src/main/scala/uk/kamchatka/fpis/Chapter02.scala
+++ src/main/scala/uk/kamchatka/fpis/Chapter02.scala
@@ -5,9 +5,9 @@ import scala.annotation.tailrec
 object Chapter02 {
   def fibonacci(n: Int): Long = {
     @tailrec
-    def fib(prev: Long, preprev: Long, n: Int): Long =
-      if (n == 0) prev
-      else fib(prev + preprev, prev, n - 1)
+    def fib(current: Long, prev: Long, n: Int): Long =
+      if (n == 0) current
+      else fib(current + prev, current, n - 1)
 
     if (n <= 0) 0
     else fib(1, 0, n - 1)
