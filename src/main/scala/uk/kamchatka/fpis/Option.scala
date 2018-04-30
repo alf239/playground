cefbdaee1a56b97714ed38ed53ec7a439d894f32
Alexey Filippov <afilippov@acm.org>
2018-04-30 08:17:53 +0100

    FPiS ch.4 Option, lift and map3

--- src/main/scala/uk/kamchatka/fpis/Option.scala
+++ src/main/scala/uk/kamchatka/fpis/Option.scala
@@ -39,8 +39,14 @@ case class Some[+A](a: A) extends Option[A] {
 object Option {
   def none[A]: Option[A] = None
 
+  def lift[A, B](a: Option[A], b: Option[B])(f: A => B): Option[B] =
+    for (x <- a) yield f(x)
+
   def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
     for (x <- a; y <- b) yield f(x, y)
+
+  def map3[A, B, C, D](a: Option[A], b: Option[B], c: Option[C])(f: (A, B, C) => D): Option[D] =
+    for (x <- a; y <- b; z <- c) yield f(x, y, z)
 
   def sequence[A](as: List[Option[A]]): Option[List[A]] = traverse(as)(identity)
 
