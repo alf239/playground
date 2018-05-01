ae976573d69292c235266a9f9dd76414659b926f
Alexey Filippov <afilippov@acm.org>
2018-05-01 18:25:41 +0100

    FPiS ch.6 State combinators

--- src/main/scala/uk/kamchatka/fpis/State.scala
+++ src/main/scala/uk/kamchatka/fpis/State.scala
@@ -19,6 +19,18 @@ object State {
   def unit[S, A](a: A): State[S, A] =
     State(s => (a, s))
 
+  def get[S]: State[S, S] =
+    State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] =
+    State(_ => ((), s))
+
+  def modify[S](f: S => S): State[S, Unit] =
+    for {
+      s <- get
+      _ <- set(f(s))
+    } yield ()
+
   def sequence[S, A](rs: List[State[S, A]]): State[S, List[A]] =
     State(s => rs.foldRight((Nil: List[A], s)) {
       case (st, (as, s0)) =>
