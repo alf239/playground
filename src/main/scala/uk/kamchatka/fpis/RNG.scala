d2580150707c03998d08363feaf4650ce2f09a23
Alexey Filippov <afilippov@acm.org>
2018-05-01 08:04:02 +0100

    FPiS ch.6 generators before mapping

--- src/main/scala/uk/kamchatka/fpis/RNG.scala
+++ src/main/scala/uk/kamchatka/fpis/RNG.scala
@@ -1,6 +1,6 @@
 package uk.kamchatka.fpis
 
-import uk.kamchatka.fpis.RNG.nonNegative
+import uk.kamchatka.fpis.RNG._
 
 trait RNG {
   def nextInt: (Int, RNG)
@@ -9,6 +9,37 @@ trait RNG {
     val (n, rng) = nextInt
     (nonNegative(n), rng)
   }
+
+  def nextDouble: (Double, RNG) = {
+    val (n, rng) = nextInt
+    (toDouble(n), rng)
+  }
+
+  def intDouble: ((Int, Double), RNG) = {
+    val (n, rng0) = this.nextInt
+    val (d, rng1) = rng0.nextDouble
+    ((n, d), rng1)
+  }
+
+  def doubleInt: ((Double, Int), RNG) = {
+    val ((n, d), rng) = intDouble
+    ((d, n), rng)
+  }
+
+  def double3: ((Double, Double, Double), RNG) = {
+    val (d0, rng0) = this.nextDouble
+    val (d1, rng1) = rng0.nextDouble
+    val (d2, rng2) = rng1.nextDouble
+    ((d0, d1, d2), rng2)
+  }
+
+  def ints(n: Int): (List[Int], RNG) =
+    if (n <= 0) (Nil, this)
+    else {
+      val (m0, rng0) = this.nextInt
+      val (ms, rng1) = rng0.ints(n - 1)
+      (m0 :: ms, rng1)
+    }
 }
 
 case class SimpleRNG(seed: Long) extends RNG {
@@ -21,5 +52,22 @@ case class SimpleRNG(seed: Long) extends RNG {
 }
 
 object RNG {
+  type Rand[+A] = RNG => (A, RNG)
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+
   def nonNegative(n: Int): Int = if (n >= 0) n else ~n
+
+  private val Range: Double = Int.MaxValue.toDouble + 1.0
+
+  def toDouble(n: Int): Double = nonNegative(n) / Range
+
 }
--- src/test/scala/uk/kamchatka/fpis/RngTest.scala
+++ src/test/scala/uk/kamchatka/fpis/RngTest.scala
@@ -2,19 +2,28 @@ package uk.kamchatka.fpis
 
 import org.scalatest.FunSpec
 import org.scalatest.prop.PropertyChecks
+import uk.kamchatka.fpis.RNG.{nonNegative, toDouble}
 
 class RngTest extends FunSpec with PropertyChecks {
   describe("nonNegative") {
     it("returns a nonnegative number") {
-      forAll { n: Int => assert(RNG.nonNegative(n) >= 0) }
+      forAll { n: Int => assert(nonNegative(n) >= 0) }
     }
     it("handles Int.MinValue") {
-      assert(RNG.nonNegative(Int.MinValue) >= 0)
-      assert(RNG.nonNegative(Int.MinValue) == Int.MaxValue)
+      assert(nonNegative(Int.MinValue) >= 0)
+      assert(nonNegative(Int.MinValue) == Int.MaxValue)
     }
     it("keeps zero") {
-      assert(RNG.nonNegative(0) == 0)
-      assert(RNG.nonNegative(-1) == 0)
+      assert(nonNegative(0) == 0)
+      assert(nonNegative(-1) == 0)
+    }
+  }
+  describe("toDouble") {
+    it("returns a nonnegative number") {
+      forAll { n: Int => assert(toDouble(n) >= 0.0) }
+    }
+    it("returns a number less than 1.0") {
+      forAll { n: Int => assert(toDouble(n) < 1.0) }
     }
   }
 }
