508d02ef31ef363e9e13e998a98e772790c4e96d
Alexey Filippov <afilippov@acm.org>
2018-05-02 06:45:48 +0100

    FPiS ch.7 initial draft as in book

--- src/main/scala/uk/kamchatka/fpis/Par.scala
+++ src/main/scala/uk/kamchatka/fpis/Par.scala
@@ -1,22 +1,35 @@
 package uk.kamchatka.fpis
 
-sealed trait Par[+A]
+import java.util.concurrent.{ExecutorService, Future, TimeUnit}
 
 object Par {
 
-  case class Val[+A](a: A) extends Par[A]
+  type Par[A] = ExecutorService => Future[A]
 
-  case class Fork[+A](a: () => Par[A]) extends Par[A]
+  def unit[A](a: A): Par[A] = _ => UnitFuture(a)
 
-  case class Apply2[-A, -B, +C](a: Par[A], b: Par[B], f: (A, B) => C) extends Par[C]
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    es => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
 
-  def unit[A](a: A): Par[A] = Val(a)
-
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = Apply2(a, b, f)
-
-  def fork[A](a: => Par[A]): Par[A] = Fork(() => a)
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(() => a(es).get())
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
-  def run[A](a: Par[A]): A = ???
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+  case class UnitFuture[A](get: A) extends Future[A] {
+    override def cancel(mayInterruptIfRunning: Boolean): Boolean = false
+
+    override def isCancelled: Boolean = false
+
+    override def isDone: Boolean = true
+
+    override def get(timeout: Long, unit: TimeUnit): A = get
+  }
+
 }
